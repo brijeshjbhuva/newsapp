9f91be3796a0c7237ac4b7b9bebe1f6119ae2efb
Brijeshkumar bhuva <developer.bhuva@gmail.com>
2025-02-11 13:49:25 +0530

    - version upgraded

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -2,7 +2,7 @@ class Strings {
   static const String headlines = "HEADLINES";
 
   static String url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=e5f9087b04fa4ae38b8d8d8a5f998df2#";
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=e5f9087b04fa4ae38b8d8d8a5f998df2#";
 
   static const String error_general = "Something does\'nt seem right. Please try again later.";
   static const String error_fetch_data = "Error in fetching data";
